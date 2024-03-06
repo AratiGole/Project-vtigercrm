6ede3a53b4a0b8f8dfcbe4d7f4a1ff07d5df35e0
AratiGole <126497790+AratiGole@users.noreply.github.com>
2024-03-06 13:24:56 +0530

    Add files via upload

---
+++ RandomNo.java
@@ -0,0 +1,13 @@
+package vtigercrm;
+
+import commonUtils.JavaUtil;
+
+public class RandomNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaUtil j= new JavaUtil();
+		System.out.println(j.getRandomNumber());
+	}
+
+}
